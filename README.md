b63427fa9eee4672053b4aa806283d59989e9807
halilkosee <51232723+halilkosee@users.noreply.github.com>
2021-08-07 17:12:05 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Spring-Boot
+ Java - Spring Boot Tutorial
---
+++ demo/demo/src/main/java/com/example/demo/student/Student.java
@@ -0,0 +1,9 @@
+package com.example.demo.student;
+
+import java.util.Date;
+
+public class Student {
+    private long id;
+    private String name;
+    private Date deateOfBirt;
+}
