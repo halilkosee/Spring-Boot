2fd58292859e22ea6b273f6501da9bb670df6941
halilkosee <51232723+halilkosee@users.noreply.github.com>
2021-08-07 18:32:40 +0300

    a simple api
    demo project created. student class and studentcontroller class added.

--- demo/demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,11 +1,14 @@
 package com.example.demo;
 
+
+import com.example.demo.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @SpringBootApplication
@@ -16,7 +19,15 @@ public class DemoApplication {
 		System.out.println("halil");
 	}
 	@GetMapping
-	public List<String> hello(){
-		return List.of("hello","world");
+	public List<Student> hello(){
+		return (List<Student>) List.of(
+				new Student(
+						1L,
+						"erdem",
+						"erdem@gmail.com",
+						 LocalDate.of(2007,8,9),
+						14
+				)
+		);
 	}
 }
--- demo/demo/src/main/java/com/example/demo/student/Student.java
+++ demo/demo/src/main/java/com/example/demo/student/Student.java
@@ -1,9 +1,80 @@
 package com.example.demo.student;
 
-import java.util.Date;
+import java.time.LocalDate;
 
 public class Student {
     private long id;
     private String name;
-    private Date deateOfBirt;
+    private String email;
+    private LocalDate dateOfBirth;
+    private Integer age;
+
+    public Student() {
+    }
+
+    public Student(long id, String name, String email, LocalDate dateOfBirth, Integer age) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.dateOfBirth = dateOfBirth;
+        this.age = age;
+    }
+
+    public Student(String name, String email, LocalDate dateOfBirth, Integer age) {
+        this.name = name;
+        this.email = email;
+        this.dateOfBirth = dateOfBirth;
+        this.age = age;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", dateOfBirth=" + dateOfBirth +
+                ", age=" + age +
+                '}';
+    }
 }
