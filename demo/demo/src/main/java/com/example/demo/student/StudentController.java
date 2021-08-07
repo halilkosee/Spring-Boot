56630a4e95637038e4270b570f46d04045f0006b
halilkosee <51232723+halilkosee@users.noreply.github.com>
2021-08-07 19:12:12 +0300

    dependecy injection
    code fix

--- demo/demo/src/main/java/com/example/demo/student/StudentController.java
+++ demo/demo/src/main/java/com/example/demo/student/StudentController.java
@@ -1,5 +1,6 @@
 package com.example.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,11 +10,13 @@ import java.util.List;
 @RestController
 @RequestMapping(path = "api/v1/student")
 public class StudentController {
+
+    private final StudentService studentService;
+
+    @Autowired
     public StudentController(StudentService studentService) {
         this.studentService = studentService;
     }
-
-    private final StudentService studentService;
 
     @GetMapping
     public List<Student> getStudents() {
