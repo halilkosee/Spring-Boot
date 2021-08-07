4bc3aee88e99c163a0ab6a63159346a2f08b40f6
halilkosee <51232723+halilkosee@users.noreply.github.com>
2021-08-07 22:00:52 +0300

    saving students

---
+++ demo/demo/src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,36 @@
+package com.example.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(
+            StudentRepository repository){
+        return args -> {
+            Student halil = new Student(
+                    "halil",
+                    "halilkosee5@gmail.com",
+                    LocalDate.of(1995, Month.FEBRUARY,20),
+                    26
+            );
+
+            Student yusuf = new Student(
+                    "yusuf",
+                    "yusufkocer@gmail.com",
+                    LocalDate.of(1996, Month.OCTOBER,10),
+                    25
+            );
+            repository.saveAll(
+                    List.of(halil, yusuf)
+            );
+        };
+    }
+}
