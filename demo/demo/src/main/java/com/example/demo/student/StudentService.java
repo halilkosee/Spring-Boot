08d9d9cbf8779934fba2569202c0b84d4c7bf3fa
halilkosee <51232723+halilkosee@users.noreply.github.com>
2021-08-07 21:29:55 +0300

    JPA repository

--- demo/demo/src/main/java/com/example/demo/student/StudentService.java
+++ demo/demo/src/main/java/com/example/demo/student/StudentService.java
@@ -1,22 +1,22 @@
 package com.example.demo.student;
 
-import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.util.List;
 
-@Component
+@Service
 public class StudentService {
 
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     public List<Student> getStudents(){
-        return (List<Student>) List.of(
-                new Student(
-                        1L,
-                        "erdem",
-                        "erdem@gmail.com",
-                        LocalDate.of(2007,8,9),
-                        14
-                )
-        );
+        return studentRepository.findAll();
     }
 }
